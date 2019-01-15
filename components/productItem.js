4ff16c94978292a09cf52758e9f89d064bb221ec
mot0laura <mot0laura@gmail.com>
2019-01-15 19:01:11 +1100

    [feat] productItem

---
+++ components/productItem.js
@@ -0,0 +1,67 @@
+import { Fragment } from "react";
+import PropTypes from "prop-types";
+
+import NavLink from "./navLink";
+import Logo from "./logo";
+import withPure from "./hoc/withPure";
+
+const ProductItem = withPure(({ content }) => {
+  const productItem = (
+    <Fragment>
+      <div key={content.label} className="item-wrapper">
+        <div className="item__title">
+          <Logo />
+          <NavLink href={content.href} badge={content.badge}>
+            <h3>{content.label || "label"}</h3>
+          </NavLink>
+        </div>
+        <p>{content.desc}</p>
+      </div>
+      <style jsx>
+        {`
+          .item-wrapper {
+            width: 200px;
+            height: 70px;
+            padding: 10px;
+          }
+          .item__title {
+            display: flex;
+            flex-direction: row;
+            justify-content: flex-start;
+          }
+          h3 {
+            font-weight: 600;
+            color: black;
+            margin: 0 5px;
+            line-height: 1;
+          }
+          p {
+            padding-top: 7px;
+            font-size: 14px;
+            margin-left: 20px;
+            font-weight: normal;
+            font-style: normal;
+            font-stretch: normal;
+            line-height: normal;
+            letter-spacing: normal;
+            color: #999;
+            text-align: left;
+          }
+        `}
+      </style>
+    </Fragment>
+  );
+  return productItem;
+});
+
+ProductItem.propTypes = {
+  content: PropTypes.shape({
+    label: PropTypes.string,
+    href: PropTypes.string,
+    badge: PropTypes.element,
+    desc: PropTypes.string
+  }),
+  children: PropTypes.element
+};
+
+export default ProductItem;
---
+++ components/tests/productItem.test.js
@@ -0,0 +1,23 @@
+import { shallow, mount } from "enzyme";
+import toJSON from "enzyme-to-json";
+import ProductItem from "../productItem";
+import { PRODUCT_CONTENT } from "../../utils/navContent";
+
+describe("<ProductItem />", () => {
+  it("renders content", () => {
+    const wrapper = mount(<ProductItem content={PRODUCT_CONTENT.links[0]} />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it("renders logo", () => {
+    const wrapper = mount(<ProductItem content={PRODUCT_CONTENT.links[0]} />);
+    expect(wrapper.find("svg").props().height).toBe("20px");
+  });
+
+  it("renders content", () => {
+    const wrapper = mount(<ProductItem content={PRODUCT_CONTENT.links[0]} />);
+    expect(wrapper.find("h3").text()).toBe(PRODUCT_CONTENT.links[0].label);
+    expect(wrapper.find("p").text()).toBe(PRODUCT_CONTENT.links[0].desc);
+    expect(wrapper.find("a").props().href).toBe(PRODUCT_CONTENT.links[0].href);
+  });
+});
