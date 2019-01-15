0a20b6dca100bfa9025402b90f8f8b4a385c409f
mot0laura <mot0laura@gmail.com>
2019-01-15 17:10:22 +1100

    [feat] badge

---
+++ components/badge.js
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import WithPure from "../components/hoc/withPure";
+
+const Badge = WithPure(props => (
+  <div className="badge-container">
+    {props.children}
+    <style jsx>
+      {`
+        .badge-container {
+          display: block;
+          border-radius: 7.5px;
+          background-color: #000000;
+          font-size: 10px;
+          color: #fff;
+          text-align: center;
+          padding: 0 7px;
+          margin: 0 8px;
+          line-height: 1.5;
+          height: 15px;
+        }
+      `}
+    </style>
+  </div>
+));
+
+Badge.propTypes = {
+  children: PropTypes.element
+};
+
+export default Badge;
---
+++ components/tests/badge.test.js
@@ -0,0 +1,18 @@
+import { shallow, mount } from "enzyme";
+import toJSON from "enzyme-to-json";
+import Badge from "../badge";
+
+const fakeData = <h1>Hello</h1>;
+
+describe("<Badge />", () => {
+  it("render Badge", () => {
+    const wrapper = shallow(<Badge>{fakeData}</Badge>);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it("render text", () => {
+    const wrapper = shallow(<Badge>{fakeData}</Badge>);
+    console.log(wrapper.debug());
+    expect(wrapper.find("h1")).toBeTruthy;
+  });
+});
