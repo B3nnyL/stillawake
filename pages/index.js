c2d591681a8048c3b6fab609a8c52d484b6fa3e4
mot0laura <mot0laura@gmail.com>
2019-01-16 13:23:55 +1100

    [feat] index

--- pages/index.js
+++ pages/index.js
@@ -1,86 +1,55 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
+import DesktopNav from "../components/desktopNav";
+import MobileNav from "../components/mobileNav";
+import MediaQuery from "react-responsive";
 
 const Home = () => (
   <div>
-
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js with Jest and Enzyme!</h1>
-      <p className="description">
-        To get started with testing, edit test examples from <code>component/tests</code> and then run <code>yarn test</code>.
-      </p>
-
-      <div className="row">
-        <Link href="https://github.com/zeit/next.js#getting-started">
-          <a className="card">
-            <h3>Next.js</h3>
-            <p>Learn more about Next</p>
-          </a>
-        </Link>
-        <Link href="https://jestjs.io/docs/">
-          <a className="card">
-            <h3>Jest with react</h3>
-            <p>
-              Learn more about Jest
-            </p>
-          </a>
-        </Link>
-        <Link href="https://airbnb.io/enzyme/">
-          <a className="card">
-            <h3>Test with Enzyme</h3>
-            <p>Learn more about DOM test with Enzyme</p>
-          </a>
-        </Link>
-      </div>
+    <div>
+      <MediaQuery query="(min-width:997px)">
+        <DesktopNav />
+      </MediaQuery>
+      <MediaQuery query="(max-width: 996px)">
+        <MobileNav />
+      </MediaQuery>
     </div>
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
+    <style jsx>
+      {`
+        :global(body) {
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
+            Helvetica, sans-serif;
+          font-size: 14px;
+        }
+        :global(*) {
+          margin: 0;
+          padding: 0;
+          border: 0;
+          font-size: 100%;
+          font: inherit;
+          vertical-align: baseline;
+        }
+        :global(h2) {
+          font-weight: 500;
+          font-size: 16px;
+          margin-bottom: 4px;
+        }
+        :global(h3) {
+          font-weight: 500;
+          font-size: 15px;
+          margin-bottom: 4px;
+        }
+        :global(a) {
+          cursor: pointer;
+        }
+        :global(li) {
+          list-style: none;
+        }
+      `}
+    </style>
   </div>
-)
+);
 
-export default Home
+export default Home;
