a8aa5935ecce70ebdc09e912216c8ecd818d5421
mot0laura <mot0laura@gmail.com>
2019-01-16 18:02:54 +1100

    [chores] mock contents

---
+++ utils/caseContent.js
@@ -0,0 +1,10 @@
+export const content = {
+  title: "Seamless workflow",
+  description:
+    "What you need to do is write a simple setup file, and then connect \
+  repo to now; after this, push, build and viola!",
+  link: {
+    text: "Read blog",
+    href: "https://www.zeit.co"
+  }
+};
---
+++ utils/quoteContent.js
@@ -0,0 +1,7 @@
+export const content = {
+  text:
+    "Now enables our design system scale from zero to thousand with \
+quality",
+  author: "John Doe",
+  title: "DesignOps Manager"
+};
