a0c3eddeaf7eafd4cca5385724b0cab47c539071
mot0laura <mot0laura@gmail.com>
2019-01-16 18:02:10 +1100

    [feat] brand container

---
+++ components/brandContainer.js
@@ -0,0 +1,40 @@
+import Proptypes from "prop-types";
+import withPure from "./hoc/withPure";
+
+const BrandContainer = withPure(({ brands }) => {
+  return (
+    <div className="brands">
+      {brands.map(brand => (
+        <li key={brand}>
+          <div className="brand__container">{brand}</div>
+        </li>
+      ))}
+      <style jsx>
+        {`
+          .brands {
+            text-align: center;
+            width: 1018px;
+            min-height: 80px;
+            margin: 20px auto;
+            display: flex;
+            z-index: 200;
+            padding: 0px 80px;
+            justify-content: space-around;
+          }
+          .brand__container {
+            max-width: 123px;
+            max-height: 80px;
+            justify-self: center;
+            justify-content: center;
+          }
+        `}
+      </style>
+    </div>
+  );
+});
+
+BrandContainer.proptypes = {
+  brands: Proptypes.arrayOf(Proptypes.element)
+};
+
+export default BrandContainer;
