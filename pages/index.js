5eb81ba9253e8536699da866075389552909c724
mot0laura <mot0laura@gmail.com>
2019-01-16 18:03:59 +1100

    [chores] new sections added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,13 @@ import React from "react";
 import Link from "next/link";
 import DesktopNav from "../components/desktopNav";
 import MobileNav from "../components/mobileNav";
+import PageNav from "../components/pageNav";
+import Hero from "../components/hero";
+import BrandContainer from "../components/brandContainer";
+import WorkflowExample from "../components/workflowExample";
+import QuoteContainer from "../components/quoteContainer";
+import CtaContainer from "../components/ctaContainer";
+import { BRANDS } from "../utils/brands";
 import MediaQuery from "react-responsive";
 
 const Home = () => (
@@ -13,6 +20,14 @@ const Home = () => (
       <MediaQuery query="(max-width: 996px)">
         <MobileNav />
       </MediaQuery>
+      <PageNav />
+      <Hero />
+      <BrandContainer brands={BRANDS} />
+      <WorkflowExample />
+      <QuoteContainer />
+      <CtaContainer>
+        <h1>Just push and code</h1>
+      </CtaContainer>
     </div>
 
     <style jsx>
@@ -22,6 +37,7 @@ const Home = () => (
           font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
             Helvetica, sans-serif;
           font-size: 14px;
+          color: #444;
         }
         :global(*) {
           margin: 0;
@@ -31,15 +47,23 @@ const Home = () => (
           font: inherit;
           vertical-align: baseline;
         }
+        :global(h1) {
+          font-weight: 500;
+          font-size: 42px;
+          margin-bottom: 4px;
+          color: #000;
+        }
         :global(h2) {
           font-weight: 500;
           font-size: 16px;
           margin-bottom: 4px;
+          color: #000;
         }
         :global(h3) {
           font-weight: 500;
           font-size: 15px;
           margin-bottom: 4px;
+          color: #000;
         }
         :global(a) {
           cursor: pointer;
