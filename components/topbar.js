50f91917686f46456e79fb3c01723c9e2be8cda8
mot0laura <mot0laura@gmail.com>
2019-01-18 05:16:20 +1100

    [fix] topbar issue

--- components/topbar.js
+++ components/topbar.js
@@ -1,54 +1,94 @@
-import { Fragment } from "react";
+import { Fragment, PureComponent } from "react";
 import PropTypes from "prop-types";
 
 import { BLACK_COLOR, WHITE_COLOR } from "./utils/tokens";
-import withPure from "./hoc/withPure";
 
-const TopBar = withPure(({ visible, ...props }) => (
-  <Fragment>
-    <div className="bar" style={{ opacity: visible ? "1" : "0" }}>
-      <div className="bar__content">{props.children}</div>
-    </div>
-    <style jsx>
-      {`
-        .bar {
-          display: block;
-          width: 100vw;
-          padding: 5px;
-          background: ${BLACK_COLOR};
-          text-align: center;
-          font-size: 14px;
-          color: ${WHITE_COLOR};
-          overflow-x: hidden;
-          opacity: 1;
-          transition: opacity 200ms ease-in-out;
-          z-index: 2000;
-        }
+class TopBar extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { visible: true };
+  }
+  onToggleClick = e => {
+    this.setState({ visible: false });
+  };
+  render() {
+    return (
+      <Fragment>
+        <div
+          className="bar"
+          style={{ opacity: this.state.visible ? "1" : "0" }}
+        >
+          <div className="bar__content">{this.props.children}</div>
+          <div className="bar__toggle cross" onClick={this.onToggleClick}>
+            <div className="cross--left" />
+            <div className="cross--right" />
+          </div>
+        </div>
+        <style jsx>
+          {`
+            .bar {
+              display: block;
+              width: 100vw;
+              height: 20px;
+              padding: 5px;
+              background: ${BLACK_COLOR};
+              text-align: center;
+              font-size: 14px;
+              color: ${WHITE_COLOR};
+              overflow-x: hidden;
+              opacity: 1;
+              transition: opacity 200ms ease-in-out;
+              z-index: 2000;
+              position: relative;
+              top: 0;
+            }
 
-        @media screen and (max-width: 699px) {
-          .bar {
-            display: block;
-            width: 100vw;
-            padding: 5px;
-            background: ${BLACK_COLOR};
-            text-align: center;
-            font-size: 14px;
-            color: ${WHITE_COLOR};
-            overflow-x: hidden;
-            opacity: 1;
-            transition: opacity 200ms ease-in-out;
-            position: fixed;
-            z-index: 2000;
-          }
-        }
-      `}
-    </style>
-  </Fragment>
-));
+            .cross {
+              position: absolute;
+              top: 13px;
+              right: 20px;
+              width: 15px;
+              height: 15px;
+              z-index: 1500;
+            }
+
+            .cross--left {
+              border: 1px #fff solid;
+              transform: rotate(45deg);
+              top: -0.5px;
+              position: absolute;
+              width: 12px;
+            }
+            .cross--right {
+              border: 1px #fff solid;
+              width: 12px;
+              transform: rotate(-45deg);
+            }
+
+            @media screen and (max-width: 699px) {
+              .bar {
+                display: block;
+                width: 100vw;
+                padding: 5px;
+                background: ${BLACK_COLOR};
+                text-align: center;
+                font-size: 14px;
+                color: ${WHITE_COLOR};
+                overflow-x: hidden;
+                opacity: 1;
+                transition: opacity 200ms ease-in-out;
+                z-index: 2000;
+              }
+            }
+          `}
+        </style>
+      </Fragment>
+    );
+  }
+}
 
 TopBar.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
-  visible: PropTypes.bool.isRequired
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element])
 };
 
 export default TopBar;
