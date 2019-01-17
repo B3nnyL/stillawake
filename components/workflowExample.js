16d75d0a4bfd8996beeb2487167317debae984df
mot0laura <mot0laura@gmail.com>
2019-01-18 02:22:22 +1100

    [feat] workflow example

--- components/workflowExample.js
+++ components/workflowExample.js
@@ -2,11 +2,34 @@ import ExampleContainer from "./exampleContainer";
 import withPure from "./hoc/withPure";
 import ExampleIntroText from "./exampleIntroText";
 import { content } from "../utils/caseContent";
+import MacContainer from "./macContainer";
+import Sketch from "./illustrations/sketch";
+import Flow from "./illustrations/flow";
+import FlowContainer from "./flowContainer";
+
+const WorkflowMac = ({ partyTime }) => {
+  return (
+    <div>
+      <MacContainer
+        height="200px"
+        horizontal="center"
+        vertical="center"
+        partyTime={partyTime}
+      >
+        <Sketch mutiStep />
+      </MacContainer>
+      <FlowContainer partyTime={partyTime}>
+        <Flow />
+      </FlowContainer>
+    </div>
+  );
+};
 
 const WorkflowExample = withPure(() => (
-  <div id="workflow">
+  <div>
     <ExampleContainer
-      CompLeft={ExampleIntroText}
+      idenity="workflow"
+      CompLeft={WorkflowMac}
       CompRight={ExampleIntroText}
       contentLeft={content}
       contentRight={content}
