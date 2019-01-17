81da5d0016b8aae0159bad042e02dac48de7efe8
mot0laura <mot0laura@gmail.com>
2019-01-18 02:21:24 +1100

    [chores] content placeholder

--- utils/caseContent.js
+++ utils/caseContent.js
@@ -1,10 +1,38 @@
 export const content = {
-  title: "Seamless workflow",
+  title: "Say goodbye to heavylift",
   description:
-    "What you need to do is write a simple setup file, and then connect \
-  repo to now; after this, push, build and viola!",
-  link: {
-    text: "Read blog",
-    href: "https://www.zeit.co"
-  }
+    "Don't worry about project structure, asset storage and infrastructure. \
+    Now builds your design and deploy code from one repository at blazing fast speed \
+    as we only build the parts of your application that have changed. \
+    No matter how large your repository.",
+  link: { text: "Read blog", href: "https://www.zeit.co" }
+};
+
+export const test = {
+  title: "Test design visually",
+  description:
+    "Now renders the code, styling, and assets, \
+allows you take pixel-perfect snapshots with headless browsers.\
+ Make you 100% confidence in UI across browsers before you ship it.",
+  link: { text: "Read blog", href: "https://www.zeit.co" }
+};
+
+export const document = {
+  title: "Document flexibly",
+  description:
+    "Take control of your design document with flexible sever-side solutions,\
+     give your document with customise tooling,\
+      interactive playground and CMS APIs,\
+       allows you Reach audiences fast smoothly",
+  link: { text: "Read blog", href: "https://www.zeit.co" }
+};
+
+export const collaborate = {
+  title: "Collaborate from one place",
+  description:
+    "TWork together with developers\
+ and managers from the same cloud workplace,\
+  always get notified with updates at the same time.\
+   No more back-and-forth communication over emails and meetings.",
+  link: { text: "Read blog", href: "https://www.zeit.co" }
 };
---
+++ utils/resourceContent.js
@@ -0,0 +1,22 @@
+const RESOURCE_CONTENT = [
+  {
+    text:
+      "Cillum commodo cupidatat nostrud anim excepteur consequat nulla anim.",
+    title: "How to start a serverless design system",
+    href: "https://www.google.com"
+  },
+  {
+    text:
+      "Cillum commodo cupidatat nostrud anim excepteur consequat nulla anim.",
+    title: "How to start a serverless design system",
+    href: "https://www.google.com"
+  },
+  {
+    text:
+      "Cillum commodo cupidatat nostrud anim excepteur consequat nulla anim.",
+    title: "How to start a serverless design system",
+    href: "https://www.google.com"
+  }
+];
+
+export default RESOURCE_CONTENT;
