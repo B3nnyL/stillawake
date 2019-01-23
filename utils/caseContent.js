b2ed261824c143255d5f0d30cd98c3c1cfc693f6
mot0laura <mot0laura@gmail.com>
2019-01-23 13:19:21 +1100

    [fix] typo [update] links

--- utils/caseContent.js
+++ utils/caseContent.js
@@ -1,40 +1,44 @@
 export const content = {
   subtitle: "workflow",
-  title: "Say goodbye to heavylifting",
+  title: "Say goodbye to heavy-lifting workflow",
   description:
     "Never worry about project's setups and low-level infrastructure. \
     Build your design and deploy code from one repository at blazing fast speed. \
     No matter how large your repository.",
-  link: { text: "Read blog", href: "https://www.zeit.co" }
+  blog: { text: "Read case", href: "https://www.zeit.co" },
+  reading: { text: "Learn more", href: "https://www.zeit.co" }
 };
 
 export const test = {
   subtitle: "Test",
-  title: "Test design visually",
+  title: "Test your design visually",
   description:
     "Preview your design and styling by taking pixel-perfect snapshots with headless browsers.\
  Let you 100% confidence in UI across browsers before you ship it.",
-  link: { text: "Read blog", href: "https://www.zeit.co" }
+  blog: { text: "Read case", href: "https://www.zeit.co" },
+  resource: { text: "Follow quickstart", href: "https://www.zeit.co/tutorial" }
 };
 
 export const document = {
   subtitle: "documentation",
-  title: "Document flexibly",
+  title: "Document your design with flexibility",
   description:
-    "Take control of your design document with flexible sever-side solutions,\
+    "Take control of your document with flexible sever-side solutions,\
      give your document with customise tooling,\
       interactive playground and CMS APIs,\
        allows you Reach audiences fast smoothly",
-  link: { text: "Read blog", href: "https://www.zeit.co" }
+  blog: { text: "Read case", href: "https://www.zeit.co/blog" },
+  resource: { text: "Follow quickstart", href: "https://www.zeit.co/tutorial" }
 };
 
 export const collaborate = {
   subtitle: "collaboration",
-  title: "Collaborate from one place",
+  title: "Work from one place",
   description:
     "Work together with developers\
  and managers from the same cloud workplace,\
   always get notified with updates at the same time.\
    No more back-and-forth communication over emails and meetings.",
-  link: { text: "Read blog", href: "https://www.zeit.co" }
+  reading: { text: "Read more", href: "https://www.zeit.co" },
+  blog: { text: "Download Now Desktop", href: "#dowload" }
 };
